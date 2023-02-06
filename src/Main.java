d2f3d07aa290c9ea51ae8985629c26902d3b897a
Alexey Klopov <aaklopov@yandex.ru>
2023-02-06 21:09:23 +0300

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,6 @@
 import java.util.function.*;
 
 public class Main {
-
-
     public static void main(String[] args) {
 
         Calculator calc = Calculator.instance.get();
@@ -13,27 +11,6 @@ public class Main {
         calc.println.accept(c);
         calc.println.accept(a);
         calc.println.accept(b);
-
-    }
-
-
-    public static class Calculator {
-
-        static Supplier<Calculator> instance = Calculator::new;
-
-        BinaryOperator<Integer> plus = (x, y) -> x + y;
-        BinaryOperator<Integer> minus = (x, y) -> x - y;
-        BinaryOperator<Integer> multiply = (x, y) -> x * y;
-        BinaryOperator<Integer> devide = (x, y) -> y != 0 ? x / y : 0; //Программа выдает ошибку "деление на ноль".
-        //С помощью тернарного оператора в переменную devide добавлено условие y != 0.
-        UnaryOperator<Integer> pow = x -> x * x;
-        UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
-        Predicate<Integer> isPositive = x -> x > 0;
-        Consumer<Integer> println = System.out::println;
-
-        public Calculator() {
-
-        }
     }
 }
 
